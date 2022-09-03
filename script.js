36b55ea34580209c198a4df180a4a6ef0233e1b3
franc51 <101558212+franc51@users.noreply.github.com>
2022-09-03 15:23:06 +0300

    4

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function showIT(){
     var input1 = document.getElementById('input1').value;
     var input2 = document.getElementById('input2').value;
     var result = document.getElementById('result').innerHTML = input1 + " and  " + input2 + " loves eachother   " + n + "%";
-    if (n > 50){
+    if (n > 80){
         document.getElementById("heart").style.display = "flex";
-    } else if (n < 50 ) {
+    } else if (n > 50 ) {
+        document.getElementById("sad").style.display = "flex";
+    } else {
         document.getElementById("sad").style.display = "flex";
     }
 }
